35ddd00ccd4401b4dd4c926238be78efc4c2be5d
SUBINA M <subina.m@ad.infosys.com>
2024-03-24 16:09:20 +0530

    error message

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const searchErrMsg = 'No item Found!';
--- src/views/results.js
+++ src/views/results.js
@@ -1,32 +1,45 @@
 import icons from '../img/icons.svg';
+import { searchErrMsg } from '../constants';
 const searchResults = document.querySelector('.results');
 const searchBox = document.querySelector('.search__field');
-export function searchResultDisplay() {
-  const displayItem = state.search.results
-    .map(item => {
-      console.log(item, 'hhhh');
-      return ` 
+
+export function searchResultDisplay(results) {
+  if (results.length === 0) {
+    const errDiv = `<div class="error">
+    <div>
+      <svg>
+        <use href="${icons}#icon-alert-triangle"></use>
+      </svg>
+    </div>
+    <p>No recipes found for your query. Please try again!</p>
+  </div>`;
+    searchResults.innerHTML = '';
+    searchResults.insertAdjacentHTML('afterbegin', errDiv);
+  } else {
+    //console.log(results, 'ggfd');
+    const displayItem = results
+      .map(item => {
+        console.log(item, 'hhhh');
+        return ` 
 <li class="preview">
-        <a class="preview__link preview__link--active" href="#${item.id}">
+        <a class="preview__link" href="#${item.id}">
           <figure class="preview__fig">
-            <img src="${item.image}" alt="Test" />
+            <img src="${item.image}" alt="${item.title}" />
           </figure>
           <div class="preview__data">
-            <h4 class="preview__title">${item.title} ...</h4>
+            <h4 class="preview__title">${item.title}</h4>
             <p class="preview__publisher">${item.publisher}</p>
-            <div class="preview__user-generated">
-              <svg>
-                <use href="${icons}.svg#icon-user"></use>
-              </svg>
-            </div>
+            
           </div>
         </a>
       </li>`;
-    })
-    .join('');
-  searchResults.innerHTML = '';
-  searchResults.insertAdjacentHTML('afterbegin', displayItem);
-}
-//console.log(searchResult);
+      })
+      .join('');
+    searchResults.innerHTML = '';
+    searchResults.insertAdjacentHTML('afterbegin', displayItem);
+  }
 
-searchBox.value = '';
+  //console.log(searchResult);
+
+  searchBox.value = '';
+}
